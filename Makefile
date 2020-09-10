40a19f65e63d836ae289bc2a239c88989bf77b11
Jim Huang <jserv.tw@gmail.com>
2020-09-10 22:28:05 +0800

    Make use of sndfile/soxr for flexible WAVE support
    WAVE file can be manipulated via sndfile, and soxr is used for sample
    rate conversion.

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 CFLAGS += -Iinclude -g -Wall --warn-vla
 CFLAGS += -O3
 CFLAGS += -march=native -funroll-loops
+CFLAGS_sndfile = $(shell pkg-config --cflags sndfile soxr)
+LDFLAGS_sndfile = $(shell pkg-config --libs sndfile soxr)
 LDFLAGS += -lm
 
 OBJS = \
@@ -21,30 +23,34 @@ src/denoise_training: CPPFLAGS+=-DTRAINING=1
 src/denoise_training: clean $(OBJS)
 	$(CC) $(OBJS) -o $@ $(LDFLAGS)
 
+examples/rnnoise_demo.o: CFLAGS+=$(CFLAGS_sndfile)
+examples/rnnoise_demo: LDFLAGS+=$(LDFLAGS_sndfile)
 examples/rnnoise_demo: examples/rnnoise_demo.o librnnoise.a
 	$(CC) -o $@ $^ $(LDFLAGS)
 
 examples/bertool: examples/bertool.o
 	$(CC) -o $@ $^ $(LDFLAGS)
 
-.PHONY: clean indent scan
-
-clean:
-	$(RM) $(OBJS) examples/*.o *.a src/denoise_training examples/rnnoise_demo
-	$(RM) gperf.svg
-
 scan:
 	scan-build $(MAKE) clean all
 
 gperf: LDFLAGS += -lprofiler -ltcmalloc
 gperf: clean examples/rnnoise_demo examples/bertool
-	CPUPROFILE_FREQUENCY=100000 CPUPROFILE=gperf.prof examples/rnnoise_demo sample.raw clean.raw
+	CPUPROFILE_FREQUENCY=100000 CPUPROFILE=gperf.prof examples/rnnoise_demo sample.wav clean.wav
 	pprof ./examples/rnnoise_demo gperf.prof --callgrind > callgrind.gperf
 	gprof2dot --format=callgrind callgrind.gperf -z main | dot -T svg > gperf.svg
+	ffmpeg -i clean.wav -ac 1 -ar 48000 -f s16le -acodec pcm_s16le clean.raw
 	examples/bertool clean.raw target.raw
 
 valgrind:
-	valgrind examples/rnnoise_demo sample_short.raw /dev/null
+	valgrind examples/rnnoise_demo sample_short.wav /dev/null
 
-%.wav: %.raw
-	sox -r 48k -e signed -b 16 -c 1 $^ $@
+check: sample.wav examples/rnnoise_demo
+	examples/rnnoise_demo $< clean.wav
+
+.PHONY: clean
+
+clean:
+	$(RM) $(OBJS) examples/*.o *.a src/denoise_training examples/rnnoise_demo
+	$(RM) clean.raw clean.wav
+	$(RM) gperf.svg
--- examples/rnnoise_demo.c
+++ examples/rnnoise_demo.c
@@ -1,5 +1,7 @@
-/* Copyright (c) 2018 Gregor Richards
- * Copyright (c) 2017 Mozilla */
+/* Copyright (c) 2020 National Cheng Kung University, Taiwan.
+ * Copyright (c) 2018 Gregor Richards.
+ * Copyright (c) 2017 Mozilla
+ */
 /*
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions
@@ -25,13 +27,43 @@
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include <stdio.h>
+#include <sndfile.h>
+#include <sox.h>
+#include <soxr.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/time.h>
 
 #include "rnnoise.h"
 
-#define FRAME_SIZE 480
+#define DENOISE_SAMPLE_RATE 48000
+#define DENOISE_FRAMES (DENOISE_SAMPLE_RATE / 1000 * 10)
+
+size_t resample(soxr_t soxr, const int16_t *in_buf, const int in_frames,
+                int16_t *out_buf, const int out_frames) {
+  size_t odone;
+  const soxr_error_t error =
+      soxr_process(soxr, in_buf, in_frames, NULL, out_buf, out_frames, &odone);
+  return error ? -1 : odone;
+}
+
+void denoise(soxr_t soxr_in, soxr_t soxr_out, int16_t *snd_buf,
+             const int in_frames, int16_t *resample_buf, float *denoise_buf,
+             DenoiseState *st) {
+  resample(soxr_in, snd_buf, in_frames, resample_buf, DENOISE_FRAMES);
+
+  for (int i = 0; i < DENOISE_FRAMES; i++)
+    denoise_buf[i] = (float)resample_buf[i];
+
+  rnnoise_process_frame(st, denoise_buf, denoise_buf);
+
+  for (int i = 0; i < DENOISE_FRAMES; i++)
+    resample_buf[i] = denoise_buf[i];
+
+  resample(soxr_out, resample_buf, DENOISE_FRAMES, snd_buf, in_frames);
+}
 
 uint64_t usecs() {
   struct timeval tv;
@@ -40,37 +72,71 @@ uint64_t usecs() {
 }
 
 int main(int argc, char **argv) {
-  int i;
-  int first = 1;
-  float x[FRAME_SIZE];
-  FILE *f1, *fout;
   DenoiseState *st;
-  st = rnnoise_create(NULL);
-  if (argc!=3) {
+  SNDFILE *in_sf, *out_sf;
+  SF_INFO in_info = {0}, out_info = {0};
+  soxr_error_t error;
+
+  if (argc != 3) {
     fprintf(stderr, "usage: %s <noisy speech> <output denoised>\n", argv[0]);
     return 1;
   }
-  f1 = fopen(argv[1], "rb");
-  fout = fopen(argv[2], "wb");
 
+  soxr_quality_spec_t const q_spec = soxr_quality_spec(SOXR_HQ, 0);
+  soxr_io_spec_t const io_spec = soxr_io_spec(SOXR_INT16_I, SOXR_INT16_I);
+  soxr_runtime_spec_t const runtime_spec = soxr_runtime_spec(sox_false);
+
+  st = rnnoise_create(NULL);
+  in_sf = sf_open(argv[1], SFM_READ, &in_info);
+  out_info = (SF_INFO){.samplerate = in_info.samplerate,
+                       .channels = 1,
+                       .format = in_info.format};
+  out_sf = sf_open(argv[2], SFM_WRITE, &out_info);
+
+  sf_command(in_sf, SFC_SET_NORM_FLOAT, NULL, SF_FALSE);
+  sf_command(out_sf, SFC_SET_NORM_FLOAT, NULL, SF_FALSE);
+
+  const int in_frames = in_info.samplerate / 1000 * 10;
+  int16_t *snd_buf = malloc(in_frames * 2);
+  int16_t resample_buf[DENOISE_FRAMES];
+  float denoise_buf[DENOISE_FRAMES];
+
+  soxr_t soxr_in = soxr_create(in_info.samplerate, DENOISE_SAMPLE_RATE, 1,
+                               &error, &io_spec, &q_spec, &runtime_spec);
+  soxr_t soxr_out = soxr_create(DENOISE_SAMPLE_RATE, out_info.samplerate, 1,
+                                &error, &io_spec, &q_spec, &runtime_spec);
+
+  const sf_count_t remain_frames = in_info.frames % in_frames;
   uint64_t t0 = usecs();
   double runtime = 0.0;
   while (1) {
-    int16_t tmp[FRAME_SIZE];
-    fread(tmp, sizeof(int16_t), FRAME_SIZE, f1);
-    if (feof(f1)) break;
-    for (i=0;i<FRAME_SIZE;i++) x[i] = tmp[i];
-    rnnoise_process_frame(st, x, x);
-    for (i=0;i<FRAME_SIZE;i++) tmp[i] = x[i];
-    if (!first) fwrite(tmp, sizeof(int16_t), FRAME_SIZE, fout);
-    first = 0;
+    const sf_count_t read_samples = sf_readf_short(in_sf, snd_buf, in_frames);
+    if (read_samples < in_frames)
+      break;
+
+    denoise(soxr_in, soxr_out, snd_buf, in_frames, resample_buf, denoise_buf,
+            st);
+    sf_writef_short(out_sf, snd_buf, in_frames);
+    runtime += 0.01; // 480 samples at 48Khz mono -> 5ms
+  }
+  if (remain_frames > 0) {
+    memset(snd_buf + (in_frames - remain_frames), 0, remain_frames);
+    denoise(soxr_in, soxr_out, snd_buf, in_frames, resample_buf, denoise_buf,
+            st);
+    sf_writef_short(out_sf, snd_buf, remain_frames);
     runtime += 0.01; // 480 samples at 48Khz mono -> 5ms
   }
   double elapsed = (usecs() - t0) / 1000000.0;
-  fprintf(stdout, "processed %3.3f seconds in %3.3f seconds (%3.2fx realtime) \n", runtime, elapsed, runtime/elapsed);
+  fprintf(stdout,
+          "processed %3.3f seconds in %3.3f seconds (%3.2fx realtime) \n",
+          runtime, elapsed, runtime / elapsed);
 
+  free(snd_buf);
+  soxr_delete(soxr_in);
+  soxr_delete(soxr_out);
   rnnoise_destroy(st);
-  fclose(f1);
-  fclose(fout);
+
+  sf_close(in_sf);
+  sf_close(out_sf);
   return 0;
 }
